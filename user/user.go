9c5ff4f60242367236e5800e4911a1855509ef92
Matthew Penner <me@matthewp.io>
2021-03-07 11:23:03 -0700

    user: new package

---
+++ user/user.go
@@ -0,0 +1,88 @@
+//
+// Copyright (c) 2021 Matthew Penner
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+// Package user ...
+package user
+
+import (
+	"time"
+
+	"github.com/matthewpi/cosmos/internal/argon2"
+	"github.com/matthewpi/cosmos/internal/snowflake"
+)
+
+// User represents a Cosmos User.
+type User struct {
+	// ID is the user's unique identifier. (unique, crypto-secure random)
+	ID snowflake.Snowflake
+
+	// Email is the user's email address. (unique, end-user data)
+	Email string `json:"email,omitempty"`
+
+	// Password is an argon2 hash of the user's password. (technically end-user data)
+	Password string `json:"-"`
+
+	// Confirmed represents if the User has confirmed their email address.
+	Confirmed bool `json:"confirmed"`
+
+	// Locked represents if the User's account is locked.
+	Locked bool `json:"-"`
+
+	// Avatar is a hash of the User's avatar.
+	Avatar string `json:"avatar"`
+
+	// CreatedAt is a timestamp of when the account was created.
+	CreatedAt time.Time `json:"created_at,omitempty"`
+}
+
+// NewUser .
+func NewUser(email string, password []byte) (*User, error) {
+	now := time.Now()
+	u := &User{
+		ID:        snowflake.NewAtTime(now),
+		Email:     email,
+		Confirmed: false,
+		Locked:    false,
+		CreatedAt: now,
+	}
+	if password != nil {
+		if err := u.SetPassword(password); err != nil {
+			return nil, err
+		}
+	}
+	return u, nil
+}
+
+// SetPassword hashes a raw password and updates the user's password.
+func (u *User) SetPassword(password []byte) error {
+	h, err := argon2.Hash(password)
+	if err != nil {
+		return err
+	}
+	u.Password = h
+	return nil
+}
+
+// VerifyPassword takes a password and the user's hashed password and verifies the password against the hashed password.
+func (u *User) VerifyPassword(password []byte) error {
+	return argon2.Verify(password, u.Password)
+}
---
+++ user/user_test.go
@@ -0,0 +1,81 @@
+//
+// Copyright (c) 2021 Matthew Penner
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+package user_test
+
+import (
+	"testing"
+
+	"github.com/matthewpi/cosmos/user"
+)
+
+func Test_NewUser(t *testing.T) {
+	for i, tc := range []struct {
+		email          string
+		password       []byte
+		expectPassword bool
+		expectErr      error
+	}{
+		{
+			email:          "matthew@example.com",
+			password:       []byte("what a really secure password"),
+			expectPassword: true,
+			expectErr:      nil,
+		},
+		{
+			email:          "matthew@example.com",
+			password:       nil,
+			expectPassword: false,
+			expectErr:      nil,
+		},
+	} {
+		u, err := user.NewUser(tc.email, tc.password)
+
+		if tc.expectErr != nil && err == nil {
+			t.Errorf("Test #%d: Expected error return value, but got \"%v\"", i, err)
+			continue
+		}
+		if tc.expectErr == nil && err != nil {
+			t.Errorf("Test #%d: Should not have error return value, but received \"%v\"", i, err)
+			continue
+		}
+
+		// If we expected an error and received an error, skip the rest of the test cases.
+		if tc.expectErr != nil && err != nil {
+			continue
+		}
+
+		// This case just ensures that `u` cannot be nil, even though it should never be.
+		if u == nil {
+			t.Errorf("Test #%d: Expected user, received nil", i)
+			continue
+		}
+		if tc.expectPassword && u.Password == "" {
+			t.Errorf("Test #%d: Expected user to have password, none set", i)
+			continue
+		}
+		if !tc.expectPassword && u.Password != "" {
+			t.Errorf("Test #%d: Expected user to not have password, one was set", i)
+			continue
+		}
+	}
+}
